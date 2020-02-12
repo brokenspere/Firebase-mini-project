3153d93e523c642956ef49615a2ebce75b39fa77
Nattapat Tamtrakool <nattapat013@outlook.com>
2020-02-12 13:37:26 +0700

    edit file location

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, './', 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './', 'build', 'index.html'));
 });
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
